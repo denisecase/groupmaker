febb03ef9067665998550498994e870970d33273
Denise Case <dcase@nwmissouri.edu>
2016-08-28 20:46:41 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Groupmaker
+
+Assign students in a class to groups randomly. 
+
+## Installation
+
+Fork this respository or just download the code as a zipfile to get started. 
+
+Right-click on the groupmaker.html file to view the application in your browser (e.g. Chrome). 
+
+## Acknowledgements
+
+http://stackoverflow.com/questions/19351759
+http://jsfiddle.net/jfriend00/H9bLH/
+
---
+++ groupmaker.js
@@ -0,0 +1,27 @@
+var uniqueRandoms = [];
+var numRandoms =0;
+
+function makeUniqueRandom() {
+    if (!uniqueRandoms.length) {
+        for (var i = 0; i < numRandoms; i++) {
+            uniqueRandoms.push(i+1);
+        }
+    }
+    var index = Math.floor(Math.random() * uniqueRandoms.length);
+    var val = uniqueRandoms[index];
+
+    // remove the value from the array so it won't be reused
+    uniqueRandoms.splice(index, 1);
+    return val;
+
+}
+
+
+function calculate() {
+    numRandoms = document.getElementById("mainform").elements.namedItem("numstudents").value;
+    $("#results").empty();
+    for (var i = 0; i < numRandoms; i++) {
+        var rand = makeUniqueRandom();
+        $("#results").append(rand + "<br>");
+    }
+}
