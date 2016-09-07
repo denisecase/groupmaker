a920e2ba10e011b3045f2d5317ee4fb6206104b1
Denise Case <dcase@nwmissouri.edu>
2016-09-07 07:56:05 -0500

    updated

--- groupmaker.js
+++ groupmaker.js
@@ -1,10 +1,10 @@
 var uniqueRandoms = [];
-var numRandoms =0;
+var numRandoms = 0;
 
 function makeUniqueRandom() {
     if (!uniqueRandoms.length) {
         for (var i = 0; i < numRandoms; i++) {
-            uniqueRandoms.push(i+1);
+            uniqueRandoms.push(i + 1);
         }
     }
     var index = Math.floor(Math.random() * uniqueRandoms.length);
@@ -13,15 +13,15 @@ function makeUniqueRandom() {
     // remove the value from the array so it won't be reused
     uniqueRandoms.splice(index, 1);
     return val;
-
 }
-
 
 function calculate() {
     numRandoms = document.getElementById("mainform").elements.namedItem("numstudents").value;
     $("#results").empty();
+     $("#results").show();
     for (var i = 0; i < numRandoms; i++) {
         var rand = makeUniqueRandom();
         $("#results").append(rand + "<br>");
     }
+   
 }
